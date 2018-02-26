d236763ceb851a1a14e1873a4eaee5bdd7f330d5
amg <amgoncalves@users.noreply.github.com>
2018-02-26 16:12:02 -0500

    Adds barebones template.

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
 source 'https://rubygems.org'
 
+gem 'bcrypt'
 gem 'sinatra'
+gem 'sinatra-flash'
--- app.rb
+++ app.rb
@@ -1,5 +1,45 @@
 require 'sinatra'
+require 'sinatra/flash'
+require_relative 'helpers'
+
+helpers do
+  def redirect_to_original_request
+    user = session[:user]
+    flash[:notice] = 'Welcome back, #{user.name}.'
+    original_request = session[:original_request]
+    session[:original_request] = nil
+    redirect original_request
+  end
+
+  def is_authenticated?
+    return !!session[:user_id]
+  end
+end
 
 get '/' do
-  erb :index
+  erb :index, :locals => { :title => 'Welcome!' }
+end
+
+get '/login/?' do
+  erb :login, :locals => { :title => 'Login' }
+end
+
+post '/login/?' do
+  if user = User.authenticate(params)
+    session[:user] = user
+    redirect_to_original_request
+  else
+    flash[:notice] = 'User login failed.  Did you enter the correct username and password?'
+    redirect '/login'
+  end
+end
+
+get '/logout' do
+  session[:user] = nil
+  flash[:notice] = 'You have been signed out.  Goodbye!'
+  redirect '/'
+end
+
+get '/signup/?' do
+  
 end
