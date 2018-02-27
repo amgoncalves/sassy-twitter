55e163b2a2377ac24b03ce3ea9418c6b94b570e6
shuai <jinchyinx@yahoo.com>
2018-02-27 18:42:28 -0500

    updated user model file

--- models/user.rb
+++ models/user.rb
@@ -3,14 +3,14 @@ require 'mongo'
 require 'mongoid'
 
 class Profile
-	attr_reader :bio, :name, :date_joined, :location, :name
+	attr_reader :bio, :dob, :date_joined, :location, :name
 
-	def initialize(bio = "", name = "", date_joined = "", location = "", name = "")
-		@bio, @name, @date_joined, @location, @name = bio, name, date_joined, location, name
+	def initialize(bio = "", dob= "", date_joined = "", location = "", name = "")
+		@bio, @dob, @date_joined, @location, @name = bio, dob, date_joined, location, name
 	end
 
 	def mongoize 
-		[ bio, dob, date_joined, location, name]
+		[ @bio, @dob, @date_joined, @location, @name]
 	end
 
 	class << self
@@ -39,12 +39,20 @@ class User
   include BCrypt
   include Mongoid::Document
 
-  field :user_id, type: Integer
   field :handle, type: String
   field :email, type: String
   field :password, type: String
   field :APItoken, type: String
   field :Profile, type: Profile
+  field :Followed, type: Set, default: []
+  field :Following, type: Set, default: []
+  field :Tweets, type: Set, default: []
+  field :LikedTweets, type: Set, default: []
+
+  validates :handle, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
+  validates :APItoken, presence: true, uniqueness: true
 
   def self.authenticate(params = {})
     return nil if params[:handle].blank? || params[:password].blank?
