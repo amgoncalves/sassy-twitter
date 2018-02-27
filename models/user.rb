d1f740e63cd4467af0ec8e5df44768fe52addfec
shuai <jinchyinx@yahoo.com>
2018-02-26 23:17:51 -0500

    added user.rb into models

---
+++ models/user.rb
@@ -0,0 +1,47 @@
+require 'mongo'
+require 'mongoid'
+
+class Profile
+	attr_reader :bio, :name
+
+	def initialize(bio = "", name = "")
+		@bio, @name = bio, name
+	end
+
+	def mongoize 
+		[ bio, dob, date_joined, location, name]
+	end
+
+	class << self
+		def demongoize(object)
+			Profile.new(object[0], object[1])
+		end
+
+		def mongoize(object)
+			case object
+			when Profile then object.mongoize
+			else object
+			end
+		end
+
+		def evolve(object)
+			case object
+			when Profile then object.mongoize
+			else object
+			end
+
+		end
+	end
+end
+
+class User
+	include Mongoid::Document
+
+	field :user_id, type: Integer
+	field :handle, type: String
+	field :email, type: String
+	field :password, type: String
+	field :APItoken, type: String
+	field :Profile, type: Profile
+
+end
