76af76807e4e9956fb282f4464f6906f08796421
shuai <jinchyinx@yahoo.com>
2018-03-13 22:05:53 -0400

    clean up the ruby file to generate payload

--- helper/generate_payload.rb
+++ helper/generate_payload.rb
@@ -2,14 +2,6 @@ require 'json'
 require 'byebug'
 require 'csv'
 
-# payload = {}
-# payload['tweet'] = Array.new
-# File.write('tweet_load', '{'
-# CSV.foreach("./seeds/tweets.csv") do |row|
-# 	payload['tweet'].push(row[1])
-# 	byebug
-# end
-#
 filename = './seeds/tweets.csv'
 line_count = File.readlines(filename).size
 line_count = 100
@@ -34,39 +26,3 @@ File.open("tweet_load", "w+") do |f|
 	f.print(']')
 	f.print('}')
 end
-
-		
-
-
-# File.open("tweet_load", "w+") do |f|
-# 	f.print('{')
-# 	f.print('"keys":["tweet"],')
-# 	f.print('"values":[')
-# 	CSV.foreach("./seeds/tweets.csv") do |row|
-# 			f.print('["' + row[1] + '"]' + ",")
-# 			# f.print('"' + row[1] + '"' + ",")
-# 	end
-
-
-	# csv = CSV.parse(filename, :col_sep => ",", :headers => false)
-	# csv.each_with_index do |row, index|
-	# 	if index + 1 == csv.length
-	# 		 byebug
-	# 		f.puts('"' + row[1] + '"')
-	# 	else
-	# 		f.puts('"' + row[1] + '"' + ",")
-	# 	end
-	# end
-
-
-	# CSV.foreach("./seeds/tweets.csv") do |row, line_num|
-	# 	if line_num == line_count 
-	# 		byebug
-	# 		f.puts('"' + row[1] + '"')
-	# 	else
-	# 		f.puts('"' + row[1] + '"' + ",")
-	# 	end
-	# end
-	# f.print(']')
-	# f.print('}')
-# end
