8b2cb43f8bc2112890d9fcfaedcbff9c380b7527
shuai <jinchyinx@yahoo.com>
2018-04-06 14:09:27 -0400

    trivial changes, nothing important

--- controllers/follow.rb
+++ controllers/follow.rb
@@ -2,7 +2,8 @@ post '/follow' do
   targeted_id = BSON::ObjectId.from_string(params[:targeted_id])
 	login_user = get_user_from_session
 	target_user = User.where(_id: targeted_id).first
-	login_user.toggle_following(target_user._id)
+	# login_user.toggle_following(target_user._id)
+	login_user.toggle_following(targeted_id)
 	target_user.toggle_followed(login_user._id)
 
   # add all tweets of that user to current user timeline
