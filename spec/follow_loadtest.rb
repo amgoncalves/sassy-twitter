a7fabccfd503f877e92b0162b6aa0fc60f7c0e2d
shuai <jinchyinx@yahoo.com>
2018-04-07 06:05:54 -0400

    updated follow loadtest, add redis part

--- spec/follow_loadtest.rb
+++ spec/follow_loadtest.rb
@@ -1,11 +1,29 @@
 post '/loadtest/follow' do
-	target_id = BSON::ObjectId.from_string(params[:targeted_id])
-	target_user = User.where(_id: target_id).first
-
 	login_id = BSON::ObjectId.from_string(params[:login_id])
 	db_login_user = User.where(_id: target_id).first
 
+	$redis.set($currentUser, db_login_user.to_json)
+
+	target_id = BSON::ObjectId.from_string(params[:targeted_id])
+	target_user = User.where(_id: target_id).first
+
+
 	db_login_user.toggle_following(target_id)
 	target_user.toggle_followed(login_id)
+
+  tweets = target_user.tweets
+  tweets.each do |tweet_id|
+    $redis.rpush(login_user._id.to_s, tweet_id)
+  end
+
+	isfollowing = db_login_user.follow?(target_user)
+
+	@info = Hash.new
+	@info[:login_user] = db_login_user
+	@info[:target_user] = target_user
+	@info[:isfollowing] = isfollowing
+	@info[:target_tweets] = tweets
+
+	erb :user, :locals => { :title -> "#{target_user.handle}"}
 
 end
