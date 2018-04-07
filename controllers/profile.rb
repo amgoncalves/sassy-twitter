beef66833d882e29d11472dea4bf9174760ceffc
WhiteShorthair <chensi@brandeis.edu>
2018-04-07 19:17:44 -0400

    commit

--- controllers/profile.rb
+++ controllers/profile.rb
@@ -7,20 +7,20 @@ def get_targeted_user
     @targeted_id = @targeted_user._id
     @cur_user = @targeted_user
     @ntweets = @targeted_user[:tweets].length
-    if @ntweets > 0
-      @targeted_tweets = Tweet.in(_id: @targeted_user[:tweets])
-      @targeted_tweets = @targeted_tweets.reverse
-    end
+    # if @ntweets > 0
+    #   @targeted_tweets = Tweet.in(_id: @targeted_user[:tweets])
+    #   @targeted_tweets = @targeted_tweets.reverse
+    # end
 
     @nfollowed = @targeted_user[:followeds].length
-    if @nfollowed > 0
-      @targeted_followed = User.in(_id: @targeted_user[:followeds])
-    end
+    # if @nfollowed > 0
+    #   @targeted_followed = User.in(_id: @targeted_user[:followeds])
+    # end
 
     @nfollowing = @targeted_user[:followings].length
-    if @nfollowing > 0
-      @targeted_following = User.in(_id: @targeted_user[:followings])
-    end
+    # if @nfollowing > 0
+    #   @targeted_following = User.in(_id: @targeted_user[:followings])
+    # end
   else
     gTLTweet_ids = $redis.lrange($globalTL, 0, -1)
     @tweets = Tweet.in(_id: gTLTweet_ids)
--- controllers/timeline.rb
+++ controllers/timeline.rb
@@ -11,16 +11,16 @@ get '/timeline' do
     @ntweets = @targeted_user.ntweets
     @nfollowed = @targeted_user.nfolloweds
     @nfollowing = @targeted_user.nfollowings
-    if @nfollowing > 0
-      @targeted_following = User.in(_id: @targeted_user[:followings])
-    end
+    # if @nfollowing > 0
+    #   @targeted_following = User.in(_id: @targeted_user[:followings])
+    # end
 
 		# code added by Shuai at Mar 23
+    byebug
 		@info = Hash.new
-		# @info[:login_user] = @cur_user
-		@info[:login_user] = get_user_from_redis
-		@cur_user = @info[:login_user]
+		@info[:login_user] = @targeted_user
 		@info[:target_user] = @targeted_user
+    @cur_user = @info[:login_user]
     
   end
   erb :timeline, :locals => { :title => 'Home Timeline!' }
