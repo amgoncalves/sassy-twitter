de062c3175e594644e3028fb03dd19cd9526550c
shuai <jinchyinx@yahoo.com>
2018-04-07 23:26:00 -0400

    add check if id exists in follow routes

--- spec/follow_loadtest.rb
+++ spec/follow_loadtest.rb
@@ -1,38 +1,40 @@
 post '/loadtest/follow' do
 	login_id = BSON::ObjectId.from_string(params[:login_id])
-	db_login_user = User.where(_id: login_id).first
-
-	$redis.set($currentUser, db_login_user.to_json)
-
+	login_query_res = User.where(_id: login_id)
 	target_id = BSON::ObjectId.from_string(params[:targeted_id])
-	target_user = User.where(_id: target_id).first
+	target_query_res = User.where(_id: target_id)
 
+	if login_query_res.exists? and target_query_res.exists?
+		# db_login_user = User.where(_id: login_id).first
+		db_login_user = login_query_res.first
+		target_user = target_query_res.first
+		# target_user = User.where(_id: target_id).first
+		$redis.set($currentUser, db_login_user.to_json)
 
-	db_login_user.toggle_following(target_id)
-	target_user.toggle_followed(login_id)
+		db_login_user.toggle_following(target_id)
+		target_user.toggle_followed(login_id)
 
-  tweets = target_user.tweets
-  tweets.each do |tweet_id|
-    $redis.rpush(db_login_user._id.to_s, tweet_id)
-  end
+		tweets = target_user.tweets
+		tweets.each do |tweet_id|
+			$redis.rpush(db_login_user._id.to_s, tweet_id)
+		end
 
-	isfollowing = db_login_user.follow?(target_user)
+		isfollowing = db_login_user.follow?(target_user)
 
-	if target_user.ntweets > 0
-		target_tweets = Tweet.in(_id: target_user[:tweets])
-		target_tweets = target_tweets.reverse
+		if target_user.ntweets > 0
+			target_tweets = Tweet.in(_id: target_user[:tweets])
+			target_tweets = target_tweets.reverse
+		end
+
+		@info = Hash.new
+		@info[:login_user] = db_login_user
+		@info[:target_user] = target_user
+		@info[:isfollowing] = isfollowing
+		@info[:target_tweets] = target_tweets
+		@tweets = @info[:target_tweets]
+
+		erb :user, :locals => { :title => "#{target_user.handle}"}
 	end
-
-
-	@info = Hash.new
-	@info[:login_user] = db_login_user
-	@info[:target_user] = target_user
-	@info[:isfollowing] = isfollowing
-	@info[:target_tweets] = target_tweets
-	@tweets = @info[:target_tweets]
-
-	# byebug
-	erb :user, :locals => { :title => "#{target_user.handle}"}
 end
 
 post '/loadtest/user/make' do
