782b53bf9892eae3b9a709bfccbb435974e1b06e
WhiteShorthair <chensi@brandeis.edu>
2018-04-08 18:08:17 -0400

    reply loadtest

--- helper/generate_payload.rb
+++ helper/generate_payload.rb
@@ -26,3 +26,23 @@ File.open("tweet_load", "w+") do |f|
 	f.print(']')
 	f.print('}')
 end
+
+File.open("reply_load", "w+") do |f|
+  f.print('{')
+  f.print('"keys":["reply"], ')
+  f.print('"values":[')
+  line_count = 500
+  File.open(filename) do |file|
+    file.each_line.with_index do |line, index|
+      row = CSV.parse_line(line, :col_sep => ",", :headers => false)
+      if index < line_count
+        f.print('["' + row[1] + '"]' + ", ")
+      else
+        f.print('["' + row[1] + '"]')
+        break
+      end
+    end
+  end
+  f.print(']')
+  f.print('}')
+end
